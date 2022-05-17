36c8962bed40453e88bf9e0db02776ae8a8d4630
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-18 00:28:15 +0300

    Finished with color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intrvlId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  intrvlId = setInterval(colorChange, 1000);
+  startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intrvlId);
+  startButton.disabled = false;
+});
+
+function colorChange() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
