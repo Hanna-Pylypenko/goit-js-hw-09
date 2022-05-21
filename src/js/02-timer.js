18ce2d319138a0f200293fe6147c7842c7f4f195
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-22 00:33:29 +0300

    added leading zero function

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,10 +6,10 @@ const startBtn = document.querySelector('[data-start]');
 startBtn.disabled = true;
 startBtn.addEventListener('click', onBtnClick);
 
-const daysNumber = document.querySelector('[data-days]');
-const hoursNumber = document.querySelector('[data-hours]');
-const minsNumber = document.querySelector('[data-minutes]');
-const secNumber = document.querySelector('[data-seconds]');
+let daysNumber = document.querySelector('[data-days]');
+let hoursNumber = document.querySelector('[data-hours]');
+let minsNumber = document.querySelector('[data-minutes]');
+let secNumber = document.querySelector('[data-seconds]');
 
 let selectedDatesData;
 
@@ -25,7 +25,7 @@ const options = {
     if (selectedDates[0].getTime() - Date.now() > 0) {
       startBtn.disabled = false;
     } else {
-      Notiflix.Notify.failure('Please choose a date in the future');
+      Notiflix.Notify.failure('Please, choose a date in the future');
     }
   },
 };
@@ -35,8 +35,8 @@ flatpickr('#datetime-picker', options);
 function onBtnClick() {
   setInterval(() => {
     let ms = selectedDatesData - Date.now();
-
     const { days, hours, minutes, seconds } = convertMs(ms);
+
     daysNumber.textContent = days;
     hoursNumber.textContent = hours;
     minsNumber.textContent = minutes;
@@ -50,12 +50,29 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  let days = Math.floor(ms / day);
+  let hours = Math.floor((ms % day) / hour);
+  let minutes = Math.floor(((ms % day) % hour) / minute);
+  let seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+  if (days < 10) {
+    days = addLeadingZero(days);
+  }
+  if (hours < 10) {
+    hours = addLeadingZero(hours);
+  }
+  if (minutes < 10) {
+    minutes = addLeadingZero(minutes);
+  }
+  if (seconds < 10) {
+    seconds = addLeadingZero(seconds);
+  }
+  console.log({ days, hours, minutes, seconds });
   return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
 }
 
 Notiflix.Notify.init({
