595d70005ca107ede85ff4347e07075682276c2e
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-23 15:35:47 +0300

    reverted changes

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,9 @@
 import Notiflix from 'notiflix';
-
 const formRef = document.querySelector('.form');
-
 formRef.addEventListener('submit', onSubmitAction);
-
 let positionCount;
 let delayCount;
 let positions = [];
-
 function onSubmitAction(evt) {
   evt.preventDefault();
   const firstDelay = parseInt(formRef.delay.value);
@@ -17,26 +13,27 @@ function onSubmitAction(evt) {
   getPosition(promiseAmount, delay);
 
   const promises = positions.map(position => {
-    createPromise(position.positionCount, position.delayCount);
-    console.log(createPromise(position.positionCount, position.delayCount));
+    createPromise(position.positionCount, position.delayCount)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   });
-
-  Promise.all(promises);
+  Promise.all(promises).then(() => console.log(Promise.all(promises)));
   evt.currentTarget.reset();
   positions = [];
-
   function getPosition(promiseAmount, delay) {
     for (let i = 0; i < promiseAmount; i++) {
       positionCount = i + 1;
       delayCount = firstDelay + delay * i;
       positions.push({ positionCount, delayCount });
-      console.log(delayCount);
     }
-    console.log(positions);
   }
 
   function createPromise(position, delay) {
-    const promise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       const shouldResolve = Math.random() > 0.3;
       setTimeout(() => {
         if (shouldResolve) {
@@ -46,8 +43,5 @@ function onSubmitAction(evt) {
         }
       }, delay);
     });
-    return promise
-      .then(() => console.log(`✅ Fulfilled promise ${position} in ${delay}ms`))
-      .catch(() => console.log(`❌ Rejected promise ${position} in ${delay}ms`));
   }
 }
